5b08dcb1971c7e5145796868c8ba710c0938a122
Joao Pedrozo <jp.pedrozo@hotmail.com>
2021-07-05 20:12:23 -0300

    remove medico

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,13 +1,8 @@
 import { Router } from "express";
-import MedicoController from "../controllers/MedicoController";
 import ComputadorController from "../controllers/ComputadorController";
 
 const router = Router();
-const medicoController = new MedicoController();
 const computadorController = new ComputadorController();
-
-router.post("/medico/cadastrar", medicoController.cadastrar);
-router.get("/medico/listar", medicoController.listar);
 
 router.post("/computador/cadastrar", computadorController.cadastrar);
 router.get("/computador/listar", computadorController.listar);
